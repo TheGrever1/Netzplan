b6879217307749e6465f8fc6b032eb0c545600d9
TheGrever1 <104456386+TheGrever1@users.noreply.github.com>
2022-05-11 10:04:53 +0200

    Add files via upload

--- Netzplan/main.py
+++ Netzplan/main.py
@@ -1,6 +1,14 @@
 import csv, os
 
 with open(os.path.join("source", "input_n.csv"), newline="") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=";", quotechar="|")
-    for row in csvreader:
+    inputnachfolger = csv.reader(csvfile, delimiter=";", quotechar="|")
+    for row in inputnachfolger:
+        print(", ".join(row))
+
+print()
+print()
+
+with open(os.path.join("source", "input_v.csv"), newline="") as csvfile:
+    inputvorgaenger = csv.reader(csvfile, delimiter=";", quotechar="|")
+    for row in inputvorgaenger:
         print(", ".join(row))
